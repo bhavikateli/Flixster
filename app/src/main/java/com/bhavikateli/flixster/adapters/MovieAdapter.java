30707103ac969542dfe0f7fea163e8c5c7231f3d
Bhavika Teli <bhaviteli@fb.com>
2020-06-24 21:11:33 -0400

    Implemented the RecyclerView with movieAdapter. App can now display current movies with poster image and overview

---
+++ app/src/main/java/com/bhavikateli/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,74 @@
+package com.bhavikateli.flixster.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bhavikateli.flixster.R;
+import com.bhavikateli.flixster.models.Movie;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+
+    //inflates a layout from XML and returning the holder
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView =  LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+       return new ViewHolder(movieView);
+    }
+
+    //populating the data into the item through the holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
+        //get the movie at the passed in position
+        Movie movie = movies.get(position);
+        //bind movie data into the viewholder
+        holder.bind(movie);
+    }
+
+    //returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+        public void bind(Movie movie) {
+            tvTitle.setText((movie.getTitle()));
+            tvOverview.setText(movie.getOverview());
+            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+        }
+    }
+}
